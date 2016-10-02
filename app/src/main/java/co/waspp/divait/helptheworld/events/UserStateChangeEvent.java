cc7b237fdaf6e75a76992f6680a8d55b87900407
dp17 <diegoparra@tiendosqui.com>
2016-10-01 19:46:59 -0500

    - Create class to manage user preferences - Create class to handle event of user state change

---
+++ app/src/main/java/co/waspp/divait/helptheworld/events/UserStateChangeEvent.java
@@ -0,0 +1,8 @@
+package co.waspp.divait.helptheworld.events;
+
+/**
+ * Created by divait on 1/10/2016.
+ */
+
+public class UserStateChangeEvent {
+}
---
+++ app/src/main/java/co/waspp/divait/helptheworld/storage/UserPreferences.java
@@ -0,0 +1,18 @@
+package co.waspp.divait.helptheworld.storage;
+
+/**
+ * Created by divait on 1/10/2016.
+ */
+
+public class UserPreferences {
+    // Manejar sharepreference
+
+    public void changeUserState (boolean state) {
+
+    }
+
+    public boolean getUserState () {
+
+        return false;
+    }
+}
